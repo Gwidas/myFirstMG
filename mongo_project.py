28a1c887c1743dfbb3c1c73b1e831bad2032dca5
Gwidas <gwidas.hiroshi@icloud.com>
2021-12-01 15:40:46 +0000

    Created list of names

--- mongo_project.py
+++ mongo_project.py
@@ -5,7 +5,7 @@ if os.path.exists("env.py"):
 
 
 MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "myFirstDB"
+DATABASE = "myFirstDb"
 COLLECTION = "celebrities"
 
 
@@ -14,7 +14,7 @@ def mongo_connect(url):
         conn = pymongo.MongoClient(url)
         return conn
     except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB: %s") % e
+        print("Could not connect to MongoDb: %s") % e
 
 
 def show_menu():
@@ -29,11 +29,56 @@ def show_menu():
     return option
 
 
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
+    except:
+        print("Error accessing the database")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_color = input("Enter hair color > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {
+        "first": first.lower(),
+        "last": last.lower(),
+        "dob": dob,
+        "gender": gender,
+        "hair_color": hair_color,
+        "occupation": occupation,
+        "nationality": nationality
+    }
+
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
+            add_record()
         elif option == "2":
             print("You have selected option 2")
         elif option == "3":
@@ -51,4 +96,3 @@ def main_loop():
 conn = mongo_connect(MONGO_URI)
 coll = conn[DATABASE][COLLECTION]
 main_loop()
-
